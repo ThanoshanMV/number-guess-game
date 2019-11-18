94b91243ed6e7d4b61f2a1d3d243cb21b53e3e3e
ThanoshanMV <mvthanoshan9@gmail.com>
2019-11-18 22:13:44 +0530

    Adding background color and optimizing code

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let randomNumber = Math.floor(Math.random() * 100)+1; //creates a random number between 1 to 100.
+let randomNumber = Math.floor(Math.random() * 100) + 1; //creates a random number between 1 to 100.
 let turnNumber = 1; //Sets initial turn number to one.
 
 /* The below three variables are each made to store a reference to the results paragraphs in our HTML, and are used to insert values into the paragraphs later on in the code:*/
@@ -7,71 +7,67 @@ let lastResult = document.getElementById("lastResult");
 let hint = document.getElementById("hint");
 
 /*The next two variables store references to the form text input and submit button and are used to control submitting the guess later on.*/
-let guessField = document.getElementById("guess");//getting the number value from input field.
+let guessField = document.getElementById("guess"); //getting the number value from input field.
 let submitBtn = document.getElementById("submit");
 
 function checkGuess() {
   let userGuess = guessField.value; //setting the input field value to userGuess
-  if(turnNumber === 1){
+  if (turnNumber === 1) {
     guessHistory.textContent = "Previous guess: ";
   }
-  guessHistory.textContent += userGuess+" ";
+  guessHistory.textContent += userGuess + " ";
 
-  if(userGuess == randomNumber){
-      lastResult.textContent = "Congratulations! You got it right!!";
-      lastResult.style.color = "green";
-      hint.textContent = "";
-      setGameOver();
-  }
-  else if(turnNumber === 10){
-      lastResult.textContent = "Game Over!";
-      lastResult.style.color = "Red";
-      hint.textContent = "";
-      setGameOver();
-  }
-  else if(userGuess <= 0 || userGuess > 100){
+  if (userGuess == randomNumber) {
+    lastResult.textContent = "Congratulations! You got it right!!";
+    lastResult.style.color = "green";
+    hint.textContent = "";
+    setGameOver();
+  } else if (turnNumber === 10) {
+    lastResult.textContent = "Game Over!";
+    lastResult.style.color = "Red";
+    hint.textContent = "";
+    setGameOver();
+  } else if (userGuess <= 0 || userGuess > 100) {
     lastResult.textContent = "The number should between 1 and 100!!!";
     lastResult.style.color = "Red";
-  }
-  else{
-      lastResult.textContent = "Wrong. Please try again ;(";
-      lastResult.style.color = "Red";
-      if(userGuess < randomNumber){
-          hint.textContent = "Last guess was too low. Aim HIGHER!";
-      }
-      else{
-          hint.textContent = "Last guess was too high. Aim LOWER!"
-      }
+  } else {
+    lastResult.textContent = "Wrong. Please try again ;(";
+    lastResult.style.color = "Red";
+    if (userGuess < randomNumber) {
+      hint.textContent = "Last guess was too low. Aim HIGHER!";
+    } else {
+      hint.textContent = "Last guess was too high. Aim LOWER!";
+    }
   }
   guessField.value = 0;
   turnNumber++;
   guessField.focus();
 }
-submitBtn.addEventListener('click',checkGuess);
+submitBtn.addEventListener("click", checkGuess);
 
 function setGameOver() {
   guessField.disabled = true;
   submitBtn.disabled = true;
-  resetButton = document.createElement('button');
-  resetButton.textContent = 'Start new game';
+  resetButton = document.createElement("button");
+  resetButton.textContent = "Start new game";
   document.body.appendChild(resetButton);
-  resetButton.addEventListener('click', resetGame);
+  resetButton.addEventListener("click", resetGame);
 }
 function resetGame() {
-randomNumber = Math.floor(Math.random() * 100) + 1;
-turnNumber = 1;
+  randomNumber = Math.floor(Math.random() * 100) + 1;
+  turnNumber = 1;
 
-let displayResults = document.querySelectorAll('.displayResults p');
-for (let i = 0 ; i < displayResults.length ; i++) {
-    displayResults[i].textContent = '';
-}
+  let displayResults = document.querySelectorAll(".displayResults p");
+  for (let i = 0; i < displayResults.length; i++) {
+    displayResults[i].textContent = "";
+  }
 
-resetButton.parentNode.removeChild(resetButton);
+  resetButton.parentNode.removeChild(resetButton);
 
-guessField.disabled = false;
-submitBtn.disabled = false;
-guessField.value = '';
-guessField.focus();
+  guessField.disabled = false;
+  submitBtn.disabled = false;
+  guessField.value = "";
+  guessField.focus();
 
-lastResult.style.backgroundColor = 'white';
+  lastResult.style.backgroundColor = "white";
 }
